2e20d47895e8adcfa2daf1789d3c2fa0de37c179
TheLightRunner <bbcfive@163.com>
2022-02-18 16:29:41 +0800

    Add files via upload

---
+++ ReactX.js
@@ -0,0 +1,74 @@
+const ReactX = (() => {
+    let hooks = [];
+
+    let index = 0;
+    const useState = (initialValue) => {
+        const localIndex = index;
+        index++;
+        if (hooks[localIndex] == undefined) {
+            hooks[localIndex] = initialValue;
+        }
+
+        const setterFunction = (newValue) => {      
+            hooks[localIndex] = newValue;
+        }
+        
+        return [hooks[localIndex], setterFunction];
+    };
+
+    const useEffect = (callback, dependencyArray) => {
+        let hasChanged = true;
+
+        const oldDependencies = hooks[index];
+
+        if (oldDependencies) {
+            hasChanged = false;
+
+            dependencyArray.forEach((dependency, index) => {
+                const oldDependency = oldDependencies[index];
+                const areTheSame = Object.is(dependency, oldDependency);
+                if (!areTheSame) {
+                    hasChanged = true;
+                }
+            });
+        }
+
+        if (hasChanged) {
+            callback();
+        }
+
+        hooks[index] = dependencyArray;
+        index++;
+    }
+
+    const resetIndex = () => {
+        index = 0;
+    }
+
+    return {
+        useState,
+        useEffect,
+        resetIndex
+    }
+})();
+
+const { useState, useEffect, resetIndex } = ReactX;
+
+const Component = () => {
+    const [counterValue, setCounterValue] = useState(1);
+
+    useEffect(() => {
+        console.log("useEffect");
+    }, []);
+
+    if (counterValue !== 2) {
+        setCounterValue(2);
+    }
+
+    console.log('counterValue', counterValue);
+
+};
+
+Component();
+resetIndex();
+Component();
